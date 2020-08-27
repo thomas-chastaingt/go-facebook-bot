bc34315a69fd8ec6561a490bfd6204d0947c92bc
Thomas Chastaingt <thomas-chastaingt@outlook.com>
2020-08-27 10:27:19 +0200

    add simple http server

--- app.go
+++ app.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func HomeEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "hello")
+}
+
 func main() {
-	println("hello world")
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeEndpoint)
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/thomas-chastaingt/go-facebook-bot
 
 go 1.14
+
+require github.com/gorilla/mux v1.8.0
